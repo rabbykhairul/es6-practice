d3468eab546bdd2102446400521b3de9949e55b4
rabbykhairul <rabbykhairul@gmail.com>
2020-03-06 09:31:27 +0600

    explore string template in ES6

---
+++ string-template.js
@@ -0,0 +1,10 @@
+// ES6 string template can be used to display variable values inside a string. It
+// also supports multiline string without the need to write \n character.
+const firstName = "Khairul";
+const lastName = "Islam";
+console.log(`My name is ${firstName} ${lastName}.`);
+
+const info = `I'm from Narayanganj,
+Dhaka,
+Bangladesh.`;
+console.log(info);
